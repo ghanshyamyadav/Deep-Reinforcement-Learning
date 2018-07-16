3757d279cb77e14ccc1307ef31cd1a27465cfbc6
Ghanshyam.y <ghanshyam.y@media.net>
2018-07-16 22:21:47 +0530

    Project navigation

---
+++ README.md
@@ -0,0 +1,70 @@
+[//]: # (Image References)
+
+[image1]: ./images/banana.gif "Trained Agent"
+
+# Project 1: Navigation
+
+### Introduction
+
+For this project, an agent has been trained to navigate (and collect bananas!) in a large, square world.
+
+
+![Trained Agent][image1]
+
+**Action and State space**
+
+The state space consists of 37 dimensions and contains the agent's velocity, along with ray-based perception of objects around agent's forward direction.  Given this information, the agent learns how to best select actions.  Four discrete actions are available, corresponding to:
+
+- **`0`** - move forward.
+- **`1`** - move backward.
+- **`2`** - turn left.
+- **`3`** - turn right.
+
+**Goal**
+
+The goal of the agent is to smartly collect as many yellow bananas as possible while avoiding blue bananas.
+A reward of +1 is provided to the agent for collecting a yellow banana, and a reward of -1 is provided for collecting a blue banana. 
+
+The environment is considered solved if the agent gets an average score of **+13** over 100 consecutive episodes.
+
+**Algorithm**
+
+A deep neural network termed as a deep Q-network is used to train a agent that learns successful policies to solve the environment.
+
+### Getting Started
+
+1. Follow the [instructions](https://github.com/Unity-Technologies/ml-agents/blob/master/docs/Installation.md) to install Unity ML-Agents. 
+
+2. Navigate to the `p1_navigation/` folder, and run the command below to obtain a few more packages.
+  ```
+  pip3 install -r requirements.txt
+  ```
+
+3. Download the environment from one of the links below.  You need only select the environment that matches your operating system:
+    - Linux: [click here](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P1/Banana/Banana_Linux.zip)
+    - Mac OSX: [click here](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P1/Banana/Banana.app.zip)
+    - Windows (32-bit): [click here](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P1/Banana/Banana_Windows_x86.zip)
+    - Windows (64-bit): [click here](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P1/Banana/Banana_Windows_x86_64.zip)
+    
+    (_For Windows users_) Check out [this link](https://support.microsoft.com/en-us/help/827218/how-to-determine-whether-a-computer-is-running-a-32-bit-version-or-64) if you need help with determining if your computer is running a 32-bit version or 64-bit version of the Windows operating system.
+
+    If you'd like to train the agent on AWS (and have not [enabled a virtual screen](https://github.com/Unity-Technologies/ml-agents/blob/master/docs/Training-on-Amazon-Web-Service.md)), then please use [this link](https://s3-us-west-1.amazonaws.com/udacity-drlnd/P1/Banana/Banana_Linux_NoVis.zip) to obtain the environment.
+
+4. Place the file in the DRLND GitHub repository, in the `p1_navigation/` folder, and unzip (or decompress) the file. 
+
+### Instructions
+
+Follow the instructions in `Report.ipynb` to get started with training your own agent!  To use a Jupyter notebook, run the following command from the `p1_navigation/` folder:
+```
+jupyter notebook
+```
+and open `Report.ipynb` from the list of files.  Alternatively, you may prefer to work with the [JupyterLab](https://jupyterlab.readthedocs.io/en/latest/) interface.  To do this, run this command instead:
+```
+jupyter-lab
+```
+The agent can also be trained from the command line.
+From the p1_navigation folder, run the following command:
+
+```
+python3 train_agent.py  
+```
---
+++ train_agent.py
@@ -0,0 +1,93 @@
+from unityagents import UnityEnvironment
+import numpy as np
+from collections import deque
+from agent import Agent
+import matplotlib.pyplot as plt
+import torch
+fig = None
+env = UnityEnvironment(file_name="./Banana.app")
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+
+# number of agents in the environment
+print('Number of agents:', len(env_info.agents))
+
+# number of actions
+action_size = brain.vector_action_space_size
+print('Number of actions:', action_size)
+
+# examine the state space
+state = env_info.vector_observations[0]
+print('States look like:', state)
+state_size = len(state)
+print('States have length:', state_size)
+
+agent = Agent(state_size=state_size, action_size=action_size, seed=0)
+
+env_info = env.reset(train_mode=True)[brain_name] # reset the environment
+state = env_info.vector_observations[0]            # get the current state
+score = 0                                          # initialize the score
+i=0
+
+def plot_graph(scores):
+    global fig
+    fig = plt.figure(2)
+    plt.clf()
+    scores_t = torch.tensor(scores)
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Scores')
+    plt.plot(scores_t.numpy())
+
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+def dqn(n_episodes=2000, max_time_steps=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Q-Learning.
+
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_time_steps (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=True)[brain_name] # reset the environment
+        state = env_info.vector_observations[0]            # get the current state
+        score = 0
+        for t in range(max_time_steps):
+            action = agent.act(state, eps)
+            env_info = env.step(action)[brain_name]        # send the action to the environment
+            next_state = env_info.vector_observations[0]   # get the next state
+            reward = env_info.rewards[0]                   # get the reward
+            done = env_info.local_done[0]                  # see if episode has finished
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        plot_graph(scores)
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=13.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save({
+                'agent_state_dict': agent.qnetwork_local.state_dict()
+            }, 'checkpoint.pth')
+            break
+    return scores
+
+scores = dqn()
+fig.savefig('figure_score_ep.jpg')
+print("Score: {}".format(score))
+env.close()
